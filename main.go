15fea7656c5d801be2104cebbc5f13c0581feee6
Brad Parbs <brad@bradparbs.com>
2018-03-10 11:26:34 -0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shawntoffel/darksky"
+	"log"
+)
+
+func main(key string, lat float, long float) {
+	client := darksky.New(key)
+	request := darksky.ForecastRequest{}
+
+	request.Latitude = lat
+	request.Longitude = long
+
+	request.Options = darksky.ForecastRequestOptions{Exclude: "hourly,minutely"}
+
+	forecast, err := client.Forecast(request)
+	if err != nil {
+		log.Fatal("Forecast request error", err)
+	}
+
+	switch forecast.Currently.Icon {
+	case "clear-day":
+		return "☀️"
+	case "partly-cloudy-day":
+	case "partly-cloudy-night":
+		return "🌤️"
+	case "cloudy":
+		return "☁️"
+	case "rain":
+		return "🌧️"
+	case "snow":
+	case "sleet":
+		return "❄️"
+	case "wind":
+		return "🌬️"
+	case "fog":
+		return "🌫️"
+	default:
+		return "☀️"
+	}
+}
